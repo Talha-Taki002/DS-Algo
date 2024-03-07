fa13e3dbf608904f0ca3161d4f093d8f14dda0ef
S.M. Zubair Talha Taki <t123alha@gmail.com>
2024-03-07 15:19:09 +0600

    Enahnced it

--- Data Structures/Graph/dsu.cpp
+++ Data Structures/Graph/dsu.cpp
@@ -4,13 +4,12 @@
 
 namespace DisjointSetUnion {
     struct Disjoint_Set_Union {
-        int n, compo_cnt, max_size;
+        int n, compo_cnt;
         std::vector <int> dsu, size;
     
         Disjoint_Set_Union() {}
         Disjoint_Set_Union(int n) : n(n) {
             compo_cnt = n;
-            max_size = 1;
             dsu.resize(n+1); // 1-based. Can also be used as 0-based.
             iota(dsu.begin(), dsu.end(), 0);
             size.assign(n+1, 1);
@@ -25,8 +24,8 @@ namespace DisjointSetUnion {
             if (u != v) {
                 dsu[v] = u;
                 size[u] += size[v];
+                size[v] = size[u];
                 compo_cnt--;
-                max_size = std::max(max_size, size[u]);
             }
         }
     };
